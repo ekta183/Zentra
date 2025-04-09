1adf373f312723f33433eac236e64b1cbb45e7f2
ekta183 <ekta.2101032cs@iiitbh.ac.in>
2025-04-10 01:18:30 +0530

    made basic api

---
+++ backend1/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+];
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
